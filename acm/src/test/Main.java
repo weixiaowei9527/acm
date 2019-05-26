d7ccaa6ff0045f60057ff1cd48833a397bf29328
weixiaowei9527 <WWJ@wei>
2019-05-26 21:50:58 +0800

    sdgsd

--- acm/src/test/Main.java
+++ acm/src/test/Main.java
@@ -105,9 +105,14 @@ class Acm {
 		/*
 		 *冒泡排序 
 		 * 一般不用。*/
+		int t;
 		for(int i = 0; i < a.length - 1; i++) {
 			for(int j = 0; j < n - i - 1; j++) {
-				
+				if (a[j] > a[j+1]) {
+					t= a[j];
+					a[j]= a[j+1];
+					a[j+1] = t;
+				}
 			}
 		}
 	}
