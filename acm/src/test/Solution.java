8b51aa4b3c81daab8251a790d8c0bc7fac2e07c5
weixiaowei9527 <WWJ@wei>
2019-05-27 17:46:21 +0800

    dsr

--- acm/src/test/Solution.java
+++ acm/src/test/Solution.java
@@ -1,11 +1,41 @@
 package test;
 
 public class Solution {
+	public boolean Find1(int target, int[][] array) {
+		boolean flag = false;
+		if (array.length != 0) {
+			/* 判断数组是否为空 */
+			int len = array[0].length;
+			if (len != 0) {
+				int i = 0;
+				int j = len - 1;
+				while (i < array.length && j >= 0) {
+					if (array[i][j] == target) {
+						flag = true;
+						break;
+					}
+					if (array[i][j] < target) {
+						i++;
+					}
+					if (i< array.length&&array[i][j] > target) {
+						j--;
+					}
+				}
+			}
+		}
+		return flag;
+	}
+
 	public boolean Find(int target, int[][] array) {
 		boolean flag = false;
-		if (array.length!= 0) {
+		if (array.length != 0) {
+			/* 判断数组是否为空 */
 			int len = array[0].length;
 			for (int i = 0; i < array.length; i++) {
+				if (len == 0) {
+					/* 防止数组下标超界，len-1 */
+					break;
+				}
 				if (array[i][0] <= target && target <= array[i][len - 1]) {
 					for (int j = 0; j < len; j++) {
 						if (array[i][j] == target) {
@@ -18,10 +48,10 @@ public class Solution {
 		}
 		return flag;
 	}
-	
+
 	public static void main(String[] args) {
 		int[][] a = new int[9][0];
-		if (a.length!= 0) {
+		if (a.length != 0) {
 			System.out.println(a[0].length);
 		}
 	}
--- acm/src/树/Solution.java
+++ acm/src/树/Solution.java
@@ -1,13 +1,26 @@
-package test;
-class Liang{
-	
-	
+package 树;
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
 }
-public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Solution {
+    public int TreeDepth(TreeNode root) {
+    	/*求树的高度*/
+        if(root!=null){
+        	int left = TreeDepth(root.left);
+          
+            int right = TreeDepth(root.right);
+            return left > right?left+1:right+1;
+        }else{
+            return 0;
+        }
+    }
 }
